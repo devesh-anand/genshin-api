b6385ea4d54b5f4ca23714231e298f3b88331d5a
devesh-anand <contactdeveshanand@gmail.com>
2022-09-12 21:38:57 +0530

    fixing port issues

--- index.js
+++ index.js
@@ -3,5 +3,18 @@ import express from "express";
 const app = express();
 
 app.get("/", (req, res) => {
+   // can show homepage here
    res.send("Welcome to genshin-api");
+});
+
+app.get("/characters/:name", async (req, res) => {
+   res.send(req.params);
+});
+
+app.get("/character", async (req, res) => {
+   res.send({ character: "genshin-characters" });
+});
+
+app.listen(5000, () => {
+   console.log(`app on port 5k`);
 });
